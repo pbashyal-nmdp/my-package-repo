54aeb3fd7809d03abfd5515e15b682b65e15fd5a
pbashyal-nmdp <pbashyal@nmdp.org>
2022-07-06 16:46:28 -0500

    Initial Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+class User {
+    constructor() {
+        this.message = "Hello World"
+        console.log(this.message)
+    }
+
+    getUserName() {
+
+    }
+
+    getUserStatus() {
+
+    }
+}
+
+
+module.export = User;
