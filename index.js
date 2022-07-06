2e3ac08d93e5bf5aca66a5f189cbc68d0b952090
pbashyal-nmdp <pbashyal@nmdp.org>
2022-07-06 17:41:17 -0500

    Added readme

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 class User {
+    message;
+    username;
+
     constructor() {
         this.message = "Hello World"
         console.log(this.message)
     }
 
     getUserName() {
+    }
 
+    setUserName(name) {
+      this.username = name
     }
 
     getUserStatus() {
